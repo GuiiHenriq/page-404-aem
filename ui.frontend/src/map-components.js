df60d2043cc3e8d2689d5b1f8706052843660803
Guilherme <guilhermeerba@hotmail.com>
2022-10-11 16:45:45 -0300

    :tada: Initial commit

---
+++ ui.frontend/src/map-components.js
@@ -0,0 +1,20 @@
+import AppPage from './components/Page/AppPage'
+import {
+  withComponentMappingContext,
+  AllowedComponentsContainer,
+  MapTo
+} from '@mavice/aem-vue-editable-components'
+
+MapTo('vue/components/page')(withComponentMappingContext(AppPage))
+
+// Container MapTo
+MapTo('vue/components/container')(
+  withComponentMappingContext(AllowedComponentsContainer),
+  {
+    emptyLabel: 'Container',
+
+    isEmpty: function (props) {
+      return !props || !props.cqItemsOrder || props.cqItemsOrder.length === 0
+    }
+  }
+)
